e6ff0c2e0cd2a2a1bc3a122997e02cb9072286b5
Elmehdi <mehdiharakat99@gmail.com>
2020-06-09 10:05:11 +0200

    Push Project
    Signed-off-by: Elmehdi <mehdiharakat99@gmail.com>

---
+++ Food_Truck/Food_Truck/Controllers/AccueilController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Food_Truck.Models;
+using System.Data.Entity;
+
+namespace Food_Truck.Controllers
+{
+    public class AccueilController : Controller
+    {
+        // GET: test
+        public ActionResult Index()
+        {
+            foodtruckEntities db = new foodtruckEntities();
+            Accueil_Model model = new Accueil_Model();
+            model.actualites = db.Actualites.ToList();
+            var TopVentes = (from lc in db.Ligne_Commande
+                                group lc by lc.id_Produit into LcGroup
+                                select new
+                                {
+                                    produit = LcGroup.Key,
+                                    TotalVente = LcGroup.Sum(x => x.quantite),
+                                }).OrderByDescending(x=>x.TotalVente).Take(3).Select(y=>y.produit).ToList();
+
+
+            model.produits = (from p in db.Produits
+                               where TopVentes.Contains(p.id_Produit)
+                               select p).ToList();
+                               
+                               
+
+            return View(model);
+        }
+    }
+}
---
+++ Food_Truck/Food_Truck/Models/Accueil_Model.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Food_Truck.Models
+{
+    public class Accueil_Model
+    {
+        public IEnumerable<Produit> produits { get; set; }
+
+        public IEnumerable<Actualite> actualites { get; set; }
+
+    }
+}
